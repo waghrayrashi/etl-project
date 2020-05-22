b20304f3a6b377632c1fe15ea86ad7c4af0809ae
waghrayrashi <61018806+waghrayrashi@users.noreply.github.com>
2020-05-22 19:17:18 -0400

    Final code

---
+++ Resources/Table_schema.sql
@@ -0,0 +1,27 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/EEUwWG
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE  walmart_stores  (
+     zipcode  INT   NOT NULL,
+     store_count  INT   NOT NULL,
+     population  DECIMAL   NOT NULL,
+     poverty_count  DECIMAL   NOT NULL,
+     poverty_rate  DECIMAL   NOT NULL
+);
+
+CREATE TABLE  store_counts  (
+     zipcode  INT   NOT NULL,
+     store_count  INT   NOT NULL,
+    CONSTRAINT  pk_store_counts  PRIMARY KEY (
+         zipcode 
+     )
+);
+
+ALTER TABLE  walmart_stores  ADD CONSTRAINT  fk_walmart_stores_zipcode  FOREIGN KEY( zipcode )
+REFERENCES  store_counts  ( zipcode );
+
---
+++ config.py
@@ -0,0 +1,9 @@
+# Enter your API keys
+gkey = "AIzaSyBFnpvUhTHaVve5_G-A6Gzd2u_KeAn_Sz4"
+
+census_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+
+postgres_id = 'postgres'
+
+postgres_pw = "Sona1@2020"
+# 'postgresql://postgres:postgres@localhost:5432/sql_challenge'
